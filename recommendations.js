a3cfff3c4ef8663816048fd71cdcc0bf07cf6811
yanha07 <yanhaloharwad@gmail.com>
2025-03-22 13:41:38 +0530

    frontend

--- recommendations.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // **Back button functionality**
-    const backButton = document.getElementById("back-button"); // Make sure this ID exists in recommendations.html
-    if (backButton) {
-        backButton.addEventListener("click", function () {
-            window.location.href = "index1.html"; // Redirect to home
-        });
-    }
-});
